c06b7f403db739a09ba8e87ca55755fa077d3c12
nik-private <niklas@varoquier.de>
2024-09-08 21:35:45 +0200

    Init

---
+++ values.py
@@ -0,0 +1,27 @@
+GOLD = "gold"
+
+IRON = "iron"
+ORICHALCUM = "orichalcum"
+AMBER = "amber"
+BISMUTH = "bismuth"
+VERISIUM = "verisium"
+
+ORES = [IRON, ORICHALCUM, AMBER, BISMUTH, VERISIUM]
+ORES_GOLD = ORES + [GOLD]
+
+CORN = "corn"
+WHEAT = "wheat"
+PUMPKIN = "pumpkin"
+ORGOURD = "orgourd"
+ZANTHIMUM = "zanthimum"
+
+CROPS = [CORN, WHEAT, PUMPKIN, ORGOURD, ZANTHIMUM]
+
+MINING = "mining"
+FARMING = "farming"
+SMELTING = "smelting"
+MAPPING = "mapping"
+SHIPPING = "shipping"
+DISENTCHANTING = "disentchanting"
+
+SKILLS = [MINING, FARMING, SMELTING, MAPPING, SHIPPING, DISENTCHANTING]
